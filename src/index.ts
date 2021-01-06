e3b7b581d411f9700064426798dde441a68afbff
jimao <1982775886@qq.com>
2021-01-06 23:04:34 +0800

    完成

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
+import fs from "fs"
 
-console.log("你好，世界");
+const text = fs.readFileSync("bin.txt", "utf-8");
+const nums = text.trim()
+              .split(/\n/)
+              .filter((item) => item)
+              .map((item) => Number(item));
+if (nums.length != 256) {
+  console.error('二进制数字长度不是256');
+  process.exit(-1)
+}
+
+const numStrs = Array(64).fill(0).map((_, index) => {
+  const start = index * 4;
+  const numSeg = nums.slice(start, start + 4);
+  const num = numSeg[0] * 8 + numSeg[1] * 4 + numSeg[2] * 2 + numSeg[3];
+  return num.toString(16);
+});
+const priKey = numStrs.join('');
+console.log(priKey);
